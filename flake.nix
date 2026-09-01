d63047dd8a89b11cd272c11b2e269e78e19ab335
Ken Shibata <ken.shibata@kiyuri.ca>
2026-09-01 14:42:33 -0400

    add nixfmt so github actions workflow can run

--- flake.nix
+++ flake.nix
@@ -3,12 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+    systems = {
+      # https://github.com/nix-systems/nix-systems
+      url = "path:./flake.systems.nix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, flake-utils, systems, ... }: {
     nixosModules = {
       eduroam = import ./eduroam;
       wlsunset = import ./wlsunset;
     };
-  };
+  } // flake-utils.lib.eachSystem (import systems) (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixfmt
+          ];
+        };
+      }
+    );
 }
